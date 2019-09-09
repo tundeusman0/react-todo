a97887d7a0e24675a8a414b4c31deac775f55a9c
tundeUsman <tundeusman0@gmail.com>
2019-09-09 09:45:24 +0100

    corrected Mongodb spelling

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MOGODB_URL, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
