ad6a0e4c26c8db5410cab1b0a01a33f6137783d9
tundeUsman <tundeusman0@gmail.com>
2019-09-09 15:05:57 +0100

    added helmet for header

--- client/src/component/DashBoard.js
+++ client/src/component/DashBoard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { logout } from '../actions/auth';
 import { deleteSelf } from '../actions/user';
 import Todo from './Todo';
@@ -13,22 +14,32 @@ export const DashBoard = ({
   deleteSelf,
   history
 }) => (
-  <div>
-    <h1>Hello {userName}</h1>
-    <button
-      onClick={() => {
-        logout();
-        history.push('/');
-      }}
-    >
-      LogOut
-    </button>
-    <button onClick={() => deleteSelf()}>Delete Your Account</button>
-    <Link to={`edit-user/${userId}`}>Edit User</Link>
-    <Link to="/add-todo">Add Todo</Link>
-    {isAdmin && <Link to="/admin"> Admin Area</Link>}
-    <Todo />
-  </div>
+  <HelmetProvider>
+    <div>
+      <Helmet>
+        <title>{userName} React Todo app</title>
+        <meta name="description" content="A React Todo authentication App" />
+        <meta
+          name="keywords"
+          content="The react app that helps you manage your Todo list"
+        />
+      </Helmet>
+      <h1>Hello {userName}</h1>
+      <button
+        onClick={() => {
+          logout();
+          history.push('/');
+        }}
+      >
+        LogOut
+      </button>
+      <button onClick={() => deleteSelf()}>Delete Your Account</button>
+      <Link to={`edit-user/${userId}`}>Edit User</Link>
+      <Link to="/add-todo">Add Todo</Link>
+      {isAdmin && <Link to="/admin"> Admin Area</Link>}
+      <Todo />
+    </div>
+  </HelmetProvider>
 );
 
 const mapStateToProps = state => ({
