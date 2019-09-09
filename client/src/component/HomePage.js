7f92867a43dfa38ac55d09cdf5d78bed80653d9e
tundeUsman <tundeusman0@gmail.com>
2019-09-09 17:52:56 +0100

    added homepage style

--- client/src/component/HomePage.js
+++ client/src/component/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
 
 const HomePage = () => {
+  const props = useSpring({
+    opacity: 1,
+    marginTop: 0,
+    from: { opacity: 0, marginTop: -500 }
+  });
   return (
-    <div>
-      <h1>Welcome to My React Todo App</h1>
-      <h2>This app is a simple app to create a user and todo lists</h2>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Login</Link>
+    <div className="homePage">
+      <div>
+        <h1>Welcome to My React Todo App</h1>
+        <h2>This app is a simple app to create a user and todo lists</h2>
+      </div>
+      <animated.div style={props}>
+        <div className="Navbar">
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </animated.div>
     </div>
   );
 };
